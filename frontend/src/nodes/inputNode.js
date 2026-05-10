eff18025fd328bdfdb85adac838759bfda5a6012
Shifa20 <shifa20naqvi@gmail.com>
2026-05-10 13:41:21 +0530

    Refactored InputNode to use reusable BaseNode component

--- frontend/src/nodes/inputNode.js
+++ frontend/src/nodes/inputNode.js
@@ -1,7 +1,7 @@
 // inputNode.js
 
 import { useState } from 'react';
-import { Handle, Position } from 'reactflow';
+import BaseNode from './baseNode';
 
 export const InputNode = ({ id, data }) => {
   const [currName, setCurrName] = useState(data?.inputName || id.replace('customInput-', 'input_'));
@@ -16,11 +16,10 @@ export const InputNode = ({ id, data }) => {
   };
 
   return (
-    <div style={{width: 200, height: 80, border: '1px solid black'}}>
-      <div>
-        <span>Input</span>
-      </div>
-      <div>
+    <BaseNode 
+       title="Input"
+       outputs={[`${id}-value`]}
+    >
         <label>
           Name:
           <input 
@@ -29,6 +28,7 @@ export const InputNode = ({ id, data }) => {
             onChange={handleNameChange} 
           />
         </label>
+
         <label>
           Type:
           <select value={inputType} onChange={handleTypeChange}>
@@ -36,12 +36,7 @@ export const InputNode = ({ id, data }) => {
             <option value="File">File</option>
           </select>
         </label>
-      </div>
-      <Handle
-        type="source"
-        position={Position.Right}
-        id={`${id}-value`}
-      />
-    </div>
+    </BaseNode>
+      
   );
-}
+};
