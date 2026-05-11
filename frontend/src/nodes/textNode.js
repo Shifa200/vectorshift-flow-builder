88947f4997d046b3d60c668db8c675718ce887e8
Shifa20 <shifa20naqvi@gmail.com>
2026-05-12 01:20:32 +0530

    Implemented dynamic textarea resizing based on input content and Added dynamic variable parsing and state updates in textarea

--- frontend/src/nodes/textNode.js
+++ frontend/src/nodes/textNode.js
@@ -1,31 +1,46 @@
 // textNode.js
 
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import BaseNode from './baseNode';
 
 export const TextNode = ({ id, data }) => {
   const [currText, setCurrText] = useState(data?.text || '{{input}}');
+  const [variables, setVariables] = useState([]);
+  const textRef = useRef(null);
 
-  const handleTextChange = (e) => {
-    setCurrText(e.target.value);
+
+   const extractVariables = (text) => {
+    const regex = /{{\s*([a-zA-Z_$][a-zA-Z0-9_$]*)\s*}}/g;
+    const matches = [...text.matchAll(regex)];
+    return matches.map(match => match[1]);
   };
+
+   const handleTextChange = (e) => {
+    const value = e.target.value;
+    setCurrText(value);
+
+    const vars = extractVariables(value);
+    setVariables(vars);
+
+    // auto resize
+    textRef.current.style.height = "auto";
+    textRef.current.style.height = textRef.current.scrollHeight + "px";
+  };
+
 
   return (
       <BaseNode
        title="Text"
+       inputs={variables.map(v => `${id}-${v}`)}
        outputs={[`${id}-output`]}
       >
-      <div>
-        <label>
-          Text:
-          <input 
-            type="text" 
-            className="w-full mt-1 border border-gray-300 rounded-md px-2 py-1 text-sm focus:outline-none focus:ring-2 focus:ring-blue-400 transition"
-            value={currText} 
-            onChange={handleTextChange} 
-          />
-        </label>
-      </div>
+      <textarea
+        ref={textRef}
+        value={currText}
+        onChange={handleTextChange}
+        className="w-full mt-1 border border-gray-300 rounded-md px-2 py-1 text-sm focus:outline-none focus:ring-2 focus:ring-blue-400 transition"
+        rows={1}
+      />
     </BaseNode>
   );
 }
